1359117962fed4bb7a68dad1fed98f74796cefc9
Igor Tsikal <igor@cedato.com>
2018-03-01 11:12:43 +0200

    webpakc & ts

---
+++ src/api.ts
@@ -0,0 +1,11 @@
+function getWrapper(identifier: string) {
+  let wrapper = null;
+  if (identifier) {
+    wrapper = document.getElementById(identifier) as HTMLElement;
+    if (!wrapper) {
+      wrapper = document.getElementsByClassName(identifier)[0] as HTMLElement;
+    }
+  }
+
+  return wrapper;
+}
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var webpack = require("webpack");
+
+module.exports = {
+//   entry: {
+//     "index": "./examples/index.ts",
+//   },
+  output: {
+    path: __dirname +'/build/',
+    filename: '[name].js',
+    //libraryTarget: "commonjs"
+  },
+  resolve: {
+    extensions: [ '.js', '.ts', '.tsx'],
+  },
+  module: {
+    loaders: [
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
+  },
+  devtool: "source-map"
+};
